fedfd3d5b64bb1eabe7b176722139fa88dd3c00c
Léo Mora <l.mora@outlook.fr>
2022-03-16 09:48:27 +0100

    Indent code

--- tdd-tp1/trivia/test/goldenmaster-game.spec.js
+++ tdd-tp1/trivia/test/goldenmaster-game.spec.js
@@ -2,29 +2,29 @@ const seedrandom = require('seedrandom');
 require('../src/game.js');
 
 function getRndInteger(min, max, seed) {
-  let rand = seedrandom(String(seed))
-  return Math.floor(rand() * (max - min + 1) ) + min;
+	let rand = seedrandom(String(seed))
+	return Math.floor(rand() * (max - min + 1)) + min;
 }
 
 describe("Goldenmaster", () => {
-  it("should create a Goldenmaster", () => {
-    let notAWinner = false;
-    let game = new Game();
-    let numberOfPlayers = getRndInteger(2, 10, "Ready Player One")
+	it("should create a Goldenmaster", () => {
+		let notAWinner = false;
+		let game = new Game();
+		let numberOfPlayers = getRndInteger(2, 10, "Ready Player One")
 
-    for (let i = 0; i < numberOfPlayers; i++) {
-      game.add(`Player ${i}`);
-    }
+		for (let i = 0; i < numberOfPlayers; i++) {
+			game.add(`Player ${i}`);
+		}
 
-    let i = 0;
-    do {
-      game.roll(getRndInteger(0, 5, i) + 1);
-      if(getRndInteger(0, 6, i) === 3){
-        notAWinner = game.wrongAnswer();
-      }else{
-        notAWinner = game.wasCorrectlyAnswered();
-      }
-      i++;
-    } while (notAWinner);
-  });
+		let i = 0;
+		do {
+			game.roll(getRndInteger(0, 5, i) + 1);
+			if (getRndInteger(0, 6, i) === 3) {
+				notAWinner = game.wrongAnswer();
+			} else {
+				notAWinner = game.wasCorrectlyAnswered();
+			}
+			i++;
+		} while (notAWinner);
+	});
 });
